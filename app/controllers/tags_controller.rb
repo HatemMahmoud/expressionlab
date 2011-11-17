b9e316dc804e1f336696c6ac4d831c95e7c41984
Hatem Mahmoud <hatem@expressionlab.com>
2011-11-17 22:28:06 +0200

    updated tags/show to display original tag name + generated tags administration

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,6 @@
 class TagsController < ApplicationController
   def show
-    @tag = params[:id].gsub('_', '.').titleize
+    @tag = Tag.find_by_name(params[:id].gsub('_', '.').titleize).name
     @articles = Article.published.tagged_with(@tag)
   end
 end
