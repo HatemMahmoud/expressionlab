b180ae51137b646221bb044408fa0b89dd6fb0f1
Hatem Mahmoud <hatem@expressionlab.com>
2011-11-18 01:07:09 +0200

    fixed tags#show to handle not-found tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,6 @@
 class TagsController < ApplicationController
   def show
-    @tag = Tag.find_by_name(params[:id].gsub('_', '.').titleize).name
+    @tag = Tag.find_by_name(params[:id].gsub('_', '.').titleize) || params[:id]
     @articles = Article.published.tagged_with(@tag)
   end
 end
