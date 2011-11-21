00f22425177cbf23301f87e0542e78e821545bd5
Hatem Mahmoud <hatem@expressionlab.com>
2011-11-21 02:29:44 +0200

    fixed comment form and redirection

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
     @article = Article.published.find(params[:article_id])
     @comment = @article.comments.build(params[:comment])
     if @comment.save
-      redirect_to @article, notice: 'Comment was successfully created.'
+      redirect_to "#{view_context.article_path_by_date(@article)}#comment-#{@comment.id}"
     else
       @article.comments.delete(@comment)
       render 'articles/show'
