c843870b4c99fdb82a23df108143b5a6242d8ed7
Hatem Mahmoud <hatem@expressionlab.com>
2011-11-26 23:47:40 +0200

    added recent articles to home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :prepare_recent_articles
+  
+  def prepare_recent_articles
+    @recent_articles = Article.recent
+  end
 end
